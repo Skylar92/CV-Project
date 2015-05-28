72631e27ebdafc7a0580240b60af9593bc1c7000
maximgalich <maksym.galich@flexshore.com>
2015-05-28 14:20:59 +0300

    add constants module

--- app/modules/aboutMe.js
+++ app/modules/aboutMe.js
@@ -2,6 +2,8 @@ var aboutMeModule = angular.module('application.aboutMe', []);
 
 aboutMeModule.controller('aboutMeController', ['$scope', '$location', '$timeout', function ($scope, $location, $timeout) {
 
+    document.title = 'About me';
+
     $scope.goBack = function(id) {
         document.getElementById('backImage').classList.add('button-animation-back');
         document.getElementById(id).classList.add('content-animation-back');
--- app/modules/application.js
+++ app/modules/application.js
@@ -3,6 +3,7 @@
     var application = angular.module('application', [
         'ngRoute',
         'ngMaterial',
+        'application.constants',
         'application.aboutMe']);
 
     application.config(['$routeProvider', function ($routeProvider) {
@@ -48,63 +49,9 @@
 
     var defaultController = application.controller('defaultController', ['$scope', '$location', '$timeout', function ($scope, $location, $timeout) {
 
-        $scope.buttonList = [
-            {
-                id: 'aboutMe',
-                'buttonName': 'About me',
-                "ngColorTile": '#C8E6C9',
-                "ngColorFooterTile": '#2E7D32',
-                "mdRowSpan": '10',
-                "mdColSpan": '2',
-                'svgIcon': 'assets/homeView/aboutMe.svg'
-            },
-            {
-                id: 'aboutEducation',
-                'buttonName': 'About my education',
-                "ngColorTile": '#E1BEE7',
-                "ngColorFooterTile": '#7B1FA2',
-                "mdRowSpan": '5',
-                "mdColSpan": '2',
-                'svgIcon': 'assets/homeView/aboutEducation.svg'
-            },
-            {
-                id: 'aboutExperience',
-                'buttonName': 'About my experience',
-                "ngColorTile": '#C5E1A5',
-                "ngColorFooterTile": '#558B2F',
-                "mdRowSpan": '5',
-                "mdColSpan": '2',
-                'svgIcon': 'assets/homeView/aboutExperience.svg'
-            },
-            {
-                id: 'aboutWishes',
-                'buttonName': 'About my wishes',
-                "ngColorTile": '#009688',
-                "ngColorFooterTile": '#3949AB',
-                "mdRowSpan": '5',
-                "mdColSpan": '4',
-                'svgIcon': 'assets/homeView/aboutWishes.svg'
-            },
-            {
-                id: 'projects',
-                'buttonName': 'My projects',
-                "ngColorTile": '#E57373',
-                "ngColorFooterTile": '#FF4081',
-                "mdRowSpan": '5',
-                "mdColSpan": '3',
-                'svgIcon': 'assets/homeView/myProjects.svg'
-            },
-            {
-                id: 'contacts',
-                'buttonName': 'My contacts',
-                "ngColorTile": '#B2DFDB',
-                "ngColorFooterTile": '#00695C',
-                "mdRowSpan": '5',
-                "mdColSpan": '1',
-                'svgIcon': 'assets/homeView/myContact.svg'
-            }
-        ];
+        document.title = 'Welcome to my CV';
 
+        $scope.buttonList = BUTTON_LIST;
         $scope.isLoadingAnimation = false;
 
         /**
