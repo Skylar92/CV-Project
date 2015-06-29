0b8de6d5105d566d7f6217bec81a710784873437
maksym.galich <maksym.galich@flexshore.com>
2015-06-29 14:22:13 +0300

    refactoring

--- app/modules/aboutMe.js
+++ app/modules/aboutMe.js
@@ -1,66 +1,50 @@
 var aboutMeModule = angular.module('application.aboutMe', []);
 
 aboutMeModule.controller('aboutMeController',
-    ['$scope', '$location', '$timeout', '$http', function ($scope, $location, $timeout, $http) {
+    ['$scope', '$location', '$timeout', '$http', '$q', function ($scope, $location, $timeout) {
 
-    $scope.colorHeader = '#aa00ff';
-    $scope.title = 'About me';
+        $scope.colorHeader = '#aa00ff';
+        $scope.title = 'About me';
 
-    $scope.fetchContent = function(url) {
-        var response = null;
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, false);
-        xhr.onload = function (e) {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    response = xhr.responseText;
-                } else {
-                    response = '<div style="font-size: 36px; text-align: center">error get '+ url + '</div>'
-                }
+        $scope.fetchContent = function (url) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, false);
+            xhr.send(null);
+            return xhr.responseText;
+        };
+
+
+        $scope.data = [
+            {
+                id: 'banana',
+                content: $scope.fetchContent('../app/templates/aboutMe/pages/aboutMe_1.html'),
+                color: '#ab47bc'
+            },
+            {
+                id: 'orange',
+                content: $scope.fetchContent('../app/templates/aboutMe/pages/aboutMe_2.html'),
+                color: '#ba68c8'
+            },
+            {
+                id: 'apple',
+                content: $scope.fetchContent('../app/templates/aboutMe/pages/aboutMe_3.html'),
+                color: '#9c27b0'
+            },
+            {
+                id: 'mango',
+                content: $scope.fetchContent('../app/templates/aboutMe/pages/aboutMe_4.html'),
+                color: '#7b1fa2'
             }
-        };
-        xhr.onerror = function (e) {
-            response = '<div style="font-size: 36px; text-align: center">error get '+ url + '</div>'
-        };
-        xhr.send(null);
-        return response;
-    };
+        ];
 
-    console.log('>>>>>>>>>>>>>> ' + $scope.fetchContent('../app/templates/aboutEducation.html'));
+        document.title = $scope.title;
 
-    $scope.data = [
-        {
-            id : 'banana',
-            content : $scope.fetchContent('../app/templates/aboutEducation.html'),
-            color : '#ab47bc'
-        },
-        {
-            id : 'orange',
-            content : '<h1 style="color: #d6b7a9">Orange</h1>',
-            color : '#ba68c8'
-        },
-        {
-            id : 'apple',
-            content : '<h1 style="color: #d6b7a9">Apple</h1>',
-            color : '#9c27b0'
-        },
-        {
-            id : 'mango',
-            content : '<h1 style="color: #d6b7a9">Mango</h1>',
-            color : '#7b1fa2'
-        }
-    ];
-
-    document.title = $scope.title;
-
-    if (document.getElementById('content-view') === null) {
         var content = new DeclarativeContent($scope.data, $scope.colorHeader, $scope.title);
         document.getElementById('created_content').appendChild(content);
-    }
 
-    document.getElementById('created_content').addEventListener('home', function(event) {
-        $timeout(function () {
-            $location.path('/');
-        }, 1000);
-    });
-}]);
+        document.getElementById('created_content').addEventListener('home', function (event) {
+            $timeout(function () {
+                $location.path('/');
+            }, 1000);
+        });
+    }]);
